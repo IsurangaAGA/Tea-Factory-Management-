ce2a8d276287fa54a0b5787331681e0b8bf2529f
ashan1202 <agaisuranga@gmail.com>
2025-09-15 16:32:11 +0530

    first commit

---
+++ src/main/java/com/teafactory/teafactory/TeafactoryApplication.java
@@ -0,0 +1,13 @@
+package com.teafactory.teafactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeafactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeafactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teafactory/teafactory/TeafactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teafactory.teafactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeafactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
