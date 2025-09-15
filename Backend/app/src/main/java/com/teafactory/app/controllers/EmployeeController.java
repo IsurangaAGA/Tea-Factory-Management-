1dd81f953b82526c08b372b0caeb26f0788b3ba3
Sudheera Weerawardhana <stweerawardhana@gmail.com>
2025-09-15 22:52:04 +0530

    Backend of View employees part Done

---
+++ Backend/app/src/main/java/com/teafactory/app/controllers/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.teafactory.app.controllers;
+
+import com.teafactory.app.entities.Employee;
+import com.teafactory.app.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+@CrossOrigin(origins = "http://localhost:5173")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping
+    public List<Employee> getEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
