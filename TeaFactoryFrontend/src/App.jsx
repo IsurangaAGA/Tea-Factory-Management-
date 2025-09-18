996859a776f33053e1859310abfd755fb21882a0
Sudheera Weerawardhana <stweerawardhana@gmail.com>
2025-09-19 00:30:51 +0530

    Created a form to add employees and tested that.

--- TeaFactoryFrontend/src/App.jsx
+++ TeaFactoryFrontend/src/App.jsx
@@ -1,11 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import EmployeeList from "./components/employee/EmployeeList";
+import AddEmployee from "./components/employee/AddEmployee";
 
 function App() {
     return (
-        <Routes>
-            <Route path="/employees" element={<EmployeeList />} />
-        </Routes>
+        <div>
+
+            <Routes>
+                <Route path="/employees" element={<EmployeeList />} />
+                <Route path="/employees-add" element={<AddEmployee />} />
+            </Routes>
+        </div>
     );
 }
 
