a7cb3c8daba064549dd7c2cceb360e8f4cce81a1
Yasith Prashan <yasithprashan@gmail.com>
2025-09-28 19:39:22 +0530

    Revert "CRUD create part"

--- Backend/app/src/main/java/com/teafactory/app/controllers/InventoryController.java
+++ Backend/app/src/main/java/com/teafactory/app/controllers/InventoryController.java
@@ -1,47 +1,4 @@
 package com.teafactory.app.controllers;
 
-import com.teafactory.app.entities.Inventory;
-import com.teafactory.app.services.InventoryService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/inventory")
-@CrossOrigin(origins = "http://localhost:5173") // adjust for your frontend port
 public class InventoryController {
-
-    @Autowired
-    private InventoryService inventoryService;
-
-    // POST - Create
-    @PostMapping
-    public Inventory createInventory(@RequestBody Inventory inventory) {
-        return inventoryService.createInventory(inventory);
-    }
-
-    // GET - All
-    @GetMapping
-    public List<Inventory> getInventory() {
-        return inventoryService.getAllInventory();
-    }
-
-    // GET - Single by ID
-    @GetMapping("/{id}")
-    public Inventory getInventoryById(@PathVariable Long id) {
-        return inventoryService.getInventoryById(id);
-    }
-
-    // PUT - Update
-    @PutMapping("/{id}")
-    public Inventory updateInventory(@PathVariable Long id, @RequestBody Inventory inventory) {
-        return inventoryService.updateInventory(id, inventory);
-    }
-
-    // DELETE
-    @DeleteMapping("/{id}")
-    public void deleteInventory(@PathVariable Long id) {
-        inventoryService.deleteInventory(id);
-    }
 }
